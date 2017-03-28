a82c6183f10cf5e51e67875fde68c59cbd893574
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-03-28 15:28:14 -0300

    Padrão para tipo de usuário .

--- database/migrations/2017_03_28_010249_create_users_table.php
+++ database/migrations/2017_03_28_010249_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('validation_code');
-            $table->tinyInteger('user_type')->default('0');;
+            $table->tinyInteger('user_type')->default('3');;
             $table->tinyInteger('ativo')->default('0');
             $table->timestamps();
         });
