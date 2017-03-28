81010864e3d2176ddc1ac68eec1603f8de8e2148
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-03-28 15:09:52 -0300

    Modelo de dados do usuário.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Monitoriamat;
+namespace Monitoriamat\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -15,7 +15,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'login', 
+        'email', 
+        'password',
+        'validation_code',
     ];
 
     /**
@@ -24,6 +27,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
     ];
 }
