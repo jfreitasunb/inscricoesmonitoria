e6d58742fc6164f2592d32934b72e4d8ef9a3fa9
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-03-29 21:20:36 -0300

    Campo do nome e campo booleano para e-mail ativo ou não.

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
     protected $primaryKey = 'id_user';
 
     protected $fillable = [
+        'nome',
         'login', 
         'email', 
         'password',
