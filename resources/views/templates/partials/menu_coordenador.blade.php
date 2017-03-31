6635a3a63d5438b28e62a16002ff99124b19398e
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-03-30 21:39:08 -0300

    Menu para o coordenador.

---
+++ resources/views/templates/partials/menu_coordenador.blade.php
@@ -0,0 +1,20 @@
+<div class="row">
+  <nav class="navbar navbar-default col-md-6 col-md-offset-3" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bar1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="bar1">
+      <ul class="nav navbar-nav">
+        <li><a href="">Configurar Inscrição</a></li>
+        <li><a href="">Relatórios</a></li>
+        <li><a href="{{ route('auth.logout') }}">Sair</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
