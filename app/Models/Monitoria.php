1158e1805c238b1de5be626c8cd08e9e66b96beb
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-04-04 22:19:13 -0300

    Mudança no texto retornado.

--- app/Models/Monitoria.php
+++ app/Models/Monitoria.php
@@ -20,15 +20,19 @@ class Monitoria extends Authenticatable
     protected $primaryKey = 'id_monitoria';
 
     
-    public static function retorna_monitoria_ativa()
+    public static function retorna_periodo_inscricao()
     {
         $monitoria_ativa = DB::table('configura_monitoria')->orderby('id_monitoria', 'desc')->first();
 
         $data_inicio = Carbon::createFromFormat('Y-m-d', $monitoria_ativa->inicio_inscricao);
         $data_fim = Carbon::createFromFormat('Y-m-d', $monitoria_ativa->fim_inscricao);
 
-        $periodo_inscricao = $data_inicio->format('d/m/Y')." à ".$data_fim->format('d/m/Y');
+        $data_hoje = (new Carbon())->format('Y-m-d');
 
-        return $periodo_inscricao;
+        if ($data_hoje >= $data_inicio && $data_hoje <= $data_fim) {
+            return $periodo_inscricao = $data_inicio->format('d/m/Y')." à ".$data_fim->format('d/m/Y');
+        }else{
+            return $periodo_inscricao = "Inscrições encerradas ou ainda não abertas";
+        }
     }
 }
