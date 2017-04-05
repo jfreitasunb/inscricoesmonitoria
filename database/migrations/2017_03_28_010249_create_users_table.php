3ba588f865e6f2d31f019ae5a6b183342e97c62a
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-04-04 21:13:41 -0300

    Correção de erro de digitação.

--- database/migrations/2017_03_28_010249_create_users_table.php
+++ database/migrations/2017_03_28_010249_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('validation_code')->nullable();
-            $table->tinyInteger('user_type')->default('3');;
+            $table->tinyInteger('user_type')->default('3');
             $table->boolean('ativo')->default('0');
             $table->rememberToken();
             $table->timestamps();
