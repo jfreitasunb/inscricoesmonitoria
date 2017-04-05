d50572845c50dd5f9b1cf28aed7650fae851e73c
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-04-04 22:08:36 -0300

    Exibir período de inscrição.

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -13,7 +13,7 @@
       <div class="jumbotron col-xs-12">
         <div class="page-header">
           <h1 class="col-xs-12">Departamento de Matemática</h1> 
-          <h2>Inscrições para a Monitoria do MAT: periodo_inscricao</h2> 
+          <h2>Inscrições para a Monitoria do MAT: {{$periodo_inscricao}}</h2> 
         </div>
       </div>
     </div>
