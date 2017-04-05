f01fdb35e72c969147680b26247f4149cb44c531
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-04-04 22:08:14 -0300

    Mais valores para teste.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,8 @@ class DatabaseSeeder extends Seeder
 
         $configura_monitoria = ['ano_monitoria' => '2017','semestre_monitoria' => '1', 'inicio_inscricao' => '2017-04-01', 'fim_inscricao' => '2017-07-31', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
         $db_configura_monitoria = DB::table('configura_monitoria')->insert($configura_monitoria);
+
+        $configura_monitoria2 = ['ano_monitoria' => '2017','semestre_monitoria' => '2', 'inicio_inscricao' => '2017-08-01', 'fim_inscricao' => '2017-08-04', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
+        $db_configura_monitoria2 = DB::table('configura_monitoria')->insert($configura_monitoria2);
     }
 }
