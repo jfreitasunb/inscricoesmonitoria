0b2d60b031845494e3df0f92f722b372dacaecd6
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-04-05 15:44:05 -0300

    Modelo para manipular dados da monitoria.

--- app/Models/Monitoria.php
+++ app/Models/Monitoria.php
@@ -4,12 +4,12 @@ namespace Monitoriamat\Models;
 
 use DB;
 use Carbon\Carbon;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class Monitoria extends Authenticatable
+class Monitoria extends Model
 {
-    use Notifiable;
+    
 
     /**
      * The attributes that are mass assignable.
@@ -19,20 +19,40 @@ class Monitoria extends Authenticatable
     
     protected $primaryKey = 'id_monitoria';
 
-    
-    public static function retorna_periodo_inscricao()
+    protected $fillable = [
+        'ano_monitoria',
+        'semestre_monitoria', 
+        'inicio_inscricao', 
+        'fim_inscricao',
+    ];
+
+    public function retorna_monitoria_ativa()
     {
         $monitoria_ativa = DB::table('configura_monitoria')->orderby('id_monitoria', 'desc')->first();
 
-        $data_inicio = Carbon::createFromFormat('Y-m-d', $monitoria_ativa->inicio_inscricao);
-        $data_fim = Carbon::createFromFormat('Y-m-d', $monitoria_ativa->fim_inscricao);
+        return $monitoria_ativa;
+
+    }
+
+    public function retorna_periodo_inscricao()
+    {
+        $monitoria_ativa = new Monitoria();
+
+        $data_inicio = Carbon::createFromFormat('Y-m-d', $monitoria_ativa->retorna_monitoria_ativa()->inicio_inscricao);
+        $data_fim = Carbon::createFromFormat('Y-m-d', $monitoria_ativa->retorna_monitoria_ativa()->fim_inscricao);
 
         $data_hoje = (new Carbon())->format('Y-m-d');
 
         if ($data_hoje >= $data_inicio && $data_hoje <= $data_fim) {
             return $periodo_inscricao = $data_inicio->format('d/m/Y')." à ".$data_fim->format('d/m/Y');
-        }else{
-            return $periodo_inscricao = "Inscrições encerradas ou ainda não abertas";
+        }
+
+        if ($data_hoje < $data_inicio) {
+            return $periodo_inscricao = "Inscrições não está aberta";
+        }
+
+        if ($data_hoje > $data_fim) {
+            return $periodo_inscricao = "Inscrições encerradas.";
         }
     }
 }
