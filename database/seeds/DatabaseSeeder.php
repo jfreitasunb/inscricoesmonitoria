afb2c9ba4a16bfd224385501453e11c4f51187d8
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-04-06 21:48:33 -0300

    Mudança no tipo de usuário.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	$user = ['nome' => 'Coordenador','login' => 'coord', 'email' => 'coord@mat.unb.br', 'password' => bcrypt('1'), 'user_type' => '2' , 'ativo' => '1', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
+    	$user = ['nome' => 'Coordenador','login' => 'coord', 'email' => 'coord@mat.unb.br', 'password' => bcrypt('1'), 'user_type' => 'coordenador' , 'ativo' => '1', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
         $db_user = DB::table('users')->insert($user);
 
 
         $configura_monitoria = ['ano_monitoria' => '2017','semestre_monitoria' => '1', 'inicio_inscricao' => '2017-04-01', 'fim_inscricao' => '2017-07-31', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
         $db_configura_monitoria = DB::table('configura_monitoria')->insert($configura_monitoria);
 
-        $configura_monitoria2 = ['ano_monitoria' => '2017','semestre_monitoria' => '2', 'inicio_inscricao' => '2017-08-01', 'fim_inscricao' => '2017-08-04', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
-        $db_configura_monitoria2 = DB::table('configura_monitoria')->insert($configura_monitoria2);
+        // $configura_monitoria2 = ['ano_monitoria' => '2017','semestre_monitoria' => '2', 'inicio_inscricao' => '2017-08-01', 'fim_inscricao' => '2017-08-04', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
+        // $db_configura_monitoria2 = DB::table('configura_monitoria')->insert($configura_monitoria2);
 
         $lista_disciplinas = [1 => 'Matemática (Bacharelado/Licenciatura)', 2 => 'Ciências da Computação (Bacharelado/Licenciatura)', 3 => 'Estatística', 4 => 'Física (Bacharelado/Licenciatura)', 5 => 'Química (Bacharelado/Licenciatura)', 6 => 'Geologia/Geofísica', 7 => 'Engenharia (Mecânica/Elétrica/Civil/Redes/Mecatrônica/Química/Produção)', 8 => 'Outros'];
 
