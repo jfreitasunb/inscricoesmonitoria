3287babced9c192e20194daf133ac4c3fc937ece
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-04-26 21:53:51 -0300

    Exibindo erros.

--- resources/views/templates/partials/coordenador/configurar_monitoria.blade.php
+++ resources/views/templates/partials/coordenador/configurar_monitoria.blade.php
@@ -1,36 +1,40 @@
 @extends('templates.default')
 
 @section('configura_monitoria')
-  <form action="" method="POST">
+  <form action="{{ route('configura.monitoria') }}" method="POST">
     <legend>Configurar período da abertura da inscrição</legend>
     <div class="row">
       <div class='col-xs-4'>
           <div class="form-group form-inline">
-              <label for="">Início da Inscríção:</label>
+              <label for="">Início da Inscrição:</label>
               <div class='input-group date' id='inicio_inscricao'>
-                  <input type='text' class="form-control" name="inicio_inscricao"/>
-                  <span class="input-group-addon">
-                      <span class="glyphicon glyphicon-calendar"></span>
-                  </span>
+                  <input type='text' class="form-control{{ $errors->has('inicio_inscricao') ? ' has-error' : '' }}" name="inicio_inscricao"/>{{ Request::old('inicio_inscricao') ?: '' }}
+                  
               </div>
+              @if ($errors->has('inicio_inscricao'))
+                      <span class="help-block">{{ $errors->first('inicio_inscricao') }}</span>
+                @endif
           </div>
       </div>
       <div class='col-xs-4'>
           <div class="form-group form-inline">
-              <label for="">Final da Inscríção:</label>
+              <label for="">Final da Inscrição:</label>
               <div class='input-group date' id='fim_inscricao'>
-                  <input type='text' class="form-control" name="fim_inscricao"/>
+                  <input type='text' class="form-control{{ $errors->has('fim_inscricao') ? ' has-error' : '' }}" name="fim_inscricao"/>{{ Request::old('fim_inscricao') ?: '' }}
                   <span class="input-group-addon">
                       <span class="glyphicon glyphicon-calendar"></span>
                   </span>
               </div>
+              @if ($errors->has('fim_inscricao'))
+                      <span class="help-block">{{ $errors->first('fim_inscricao') }}</span>
+                @endif
           </div>
       </div>
       <div class="col-xs-4">
-        <div class="form-group form-inline">
+        <div class="form-group form-inline{{ $errors->has('semestre') ? ' has-error' : '' }}">
         <label for="">Semestre: </label>
-          <input type="radio" name="semestre" class="radio" value="1"> 1
-          <input type="radio" name="semestre" class="radio" value="2"> 2
+          <input type="radio" name="semestre" id="semestre" class="radio" value="1" @if(Request::old('semestre')) checked @endif> 1
+          <input type="radio" name="semestre" id="semestre" class="radio" value="2" @if(Request::old('semestre')) checked @endif> 2
         </div>
       </div>
     </div>
@@ -38,7 +42,10 @@
     <legend>Escolher disciplinas disponíveis para a Monitoria</legend>
     <div class="row">
       <div class="col-xs-12">
-        <div class="form-group form-horizontal">
+        <div class="form-group form-horizontal{{ $errors->has('escolhas_coordenador') ? ' has-error' : '' }}">
+        @if ($errors->has('escolhas_coordenador'))
+          <span class="help-block">Você deve escolher pelo menos 01(uma) disciplina</span>
+        @endif
           <input type="checkbox" name="selectAll" id="disciplinas" /> Selecionar todos
           <table class="table table-striped" id="disciplinas">
             <thead>
@@ -53,10 +60,10 @@
             <?php $i=0;?>
             @while ($i < sizeof($disciplinas))
                 <tr>
-                  <td><input type="checkbox" id="disciplinas" name="escolhas_coordenador[]" class="checkbox" {{ $disciplinas[$i]->codigo }}></td>
+                  <td><input type="checkbox" id="disciplinas" name="escolhas_coordenador[]" class="checkbox" value="{{ $disciplinas[$i]->codigo }}"></td>
                   <td>{{ $disciplinas[$i]->nome }}</td>
                   @if ($i+1<sizeof($disciplinas))
-                    <td><input type="checkbox" id="disciplinas" name="escolhas_coordenador[]" class="checkbox" {{ $disciplinas[$i+1]->codigo }}></td>
+                    <td><input type="checkbox" id="disciplinas" name="escolhas_coordenador[]" class="checkbox" value="{{ $disciplinas[$i+1]->codigo }}"></td>
                     <td>{{ $disciplinas[$i+1]->nome }}</td>
                   @endif
                 </tr>
@@ -77,6 +84,7 @@
             </div>
           </div>
         </div>
+        <input type="hidden" name="_token" value="{{ Session::token() }}">
   </form>
 
   <div id="result"></div>
