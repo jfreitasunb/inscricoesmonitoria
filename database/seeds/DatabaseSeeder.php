579ea886bc3f4c7b59ec6bd59faa7acd06302673
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-04-27 22:01:01 -0300

    Mudança no sede.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $db_user = DB::table('users')->insert($user);
 
 
-        $configura_monitoria = ['ano_monitoria' => '2017','semestre_monitoria' => '1', 'inicio_inscricao' => '2017-04-01', 'fim_inscricao' => '2017-07-31', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
+        $configura_monitoria = ['ano_monitoria' => '2016','semestre_monitoria' => '1', 'inicio_inscricao' => '2016-04-01', 'fim_inscricao' => '2017-06-31', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
         $db_configura_monitoria = DB::table('configura_monitoria')->insert($configura_monitoria);
 
         // $configura_monitoria2 = ['ano_monitoria' => '2017','semestre_monitoria' => '2', 'inicio_inscricao' => '2017-08-01', 'fim_inscricao' => '2017-08-04', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
