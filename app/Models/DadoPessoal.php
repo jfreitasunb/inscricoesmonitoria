a3fb601c00585b4d7017e6fd0ad36ce4e646f7ff
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-05-16 21:13:55 -0300

    Início do Model para dados pessoais.

--- app/Models/DadoPessoal.php
+++ app/Models/DadoPessoal.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class DadosPessoais extends Model
 {
-    //
+    protected $primaryKey = 'id';
+
+    protected $table = 'dados_pessoais';
+
+    protected $fillable = [
+        'numerorg',
+        'emissor', 
+        'cpf',
+        'endereco',
+        'cidade',
+        'cep',
+        'estado',
+        'telefone',
+        'celular',
+    ];
+
 }
