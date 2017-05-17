a97858c57e38b04ce4f9cde2fae6175923d90c9a
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-05-16 22:24:01 -0300

    Arrumando o template.

--- resources/views/templates/partials/candidato/dados_pessoais.blade.php
+++ resources/views/templates/partials/candidato/dados_pessoais.blade.php
@@ -16,7 +16,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="numerorg">RG</label>  
           <div class="col-md-4">
-            <input id="numerorg" name="numerorg" type="text" class="form-control input-md" required="required" {{$numerorg}}>
+            <input id="numerorg" name="numerorg" type="text" class="form-control input-md" required="required">
           </div>
         </div>
       </div>
@@ -25,7 +25,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="emissorrg">Órgão Emissor</label>  
           <div class="col-md-4">
-            <input id="emissorrg" name="emissorrg" type="text" class="form-control input-md" required="required" {{$emissorrg}}>
+            <input id="emissorrg" name="emissorrg" type="text" class="form-control input-md" required="required">
           </div>
         </div>
       </div>
@@ -34,7 +34,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="cpf">CPF</label>  
           <div class="col-md-4">
-            <input id="cpf" name="cpf" type="text" placeholder="Somente números" class="form-control input-md" required="required" {{$cpf}}>
+            <input id="cpf" name="cpf" type="text" placeholder="Somente números" class="form-control input-md" required="required">
           </div>
         </div>
       </div>
@@ -43,7 +43,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="endereco">Endereço</label>  
           <div class="col-md-4">
-            <input id="endereco" name="endereco" type="text" placeholder="" class="form-control input-md" required="" {{$endereco}}>
+            <input id="endereco" name="endereco" type="text" placeholder="" class="form-control input-md" required="">
           </div>
         </div>
       </div>
@@ -52,7 +52,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="cidade">Cidade</label>  
           <div class="col-md-4">
-            <input id="cidade" name="cidade" type="text" placeholder="" class="form-control input-md" required="" {{$cidade}}>
+            <input id="cidade" name="cidade" type="text" placeholder="" class="form-control input-md" required="">
           </div>
         </div>
       </div>
@@ -61,7 +61,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="cep">CEP</label>  
           <div class="col-md-4">
-            <input id="cep" name="cep" type="text" placeholder="" class="form-control input-md" required="" {{$cep}}>
+            <input id="cep" name="cep" type="text" placeholder="" class="form-control input-md" required="">
           </div>
         </div>
       </div>
@@ -70,7 +70,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="estado">Estado</label>  
           <div class="col-md-4">
-            <input id="estado" name="estado" type="text" placeholder="" class="form-control input-md" required="" {{$estado}}>
+            <input id="estado" name="estado" type="text" placeholder="" class="form-control input-md" required="">
           </div>
         </div>
       </div>
@@ -79,7 +79,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="telefone">Telefone</label>  
           <div class="col-md-4">
-            <input id="telefone" name="telefone" type="text" placeholder="(DD)#######" class="form-control input-md" required="" {{$telefone}}>
+            <input id="telefone" name="telefone" type="text" placeholder="(DD)#######" class="form-control input-md" required="">
           </div>
         </div>
       </div>
@@ -88,7 +88,7 @@
         <div class="row">
           <label class="col-md-4 control-label" for="celular">Celular</label>  
           <div class="col-md-4">
-            <input id="celular" name="celular" type="text" placeholder="(DD)#######" class="form-control input-md" required="" {{$celular}}>
+            <input id="celular" name="celular" type="text" placeholder="(DD)#######" class="form-control input-md" required="">
           </div>
         </div>
       </div>
@@ -97,7 +97,7 @@
       <div class="form-group">
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
-            <input type="submit" name="registrar" id="register-submit" class="btn btn-primary btn-lg" tabindex="4" value="Enviar" {{$desativa_registro}}>
+            <input type="submit" name="registrar" id="register-submit" class="btn btn-primary btn-lg" tabindex="4" value="Enviar">
           </div>
         </div>
       </div>
