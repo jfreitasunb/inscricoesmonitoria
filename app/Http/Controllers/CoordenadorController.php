4ac92f4ab2fd17534f23d1e0bd2752c337bb7cab
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-05-16 22:08:04 -0300

    Remoção de funções desnecessárias no controller do coordenador.

--- app/Http/Controllers/CoordenadorController.php
+++ app/Http/Controllers/CoordenadorController.php
@@ -93,49 +93,4 @@ class CoordenadorController extends BaseController
 		return view('templates.partials.coordenador.relatorio_monitoria');
 	}
 
-	public function getLogin()
-	{	
-		return view('auth.login');
-	}
-
-	public function postLogin(Request $request)
-	{
-		$this->validate($request, [
-			'login' => 'required',
-			'password' => 'required',
-		]);
-
-		
-		$user = DB::table('users')->where('login', $request->input('login'))->value('ativo');
-
-		if (!$user) {
-			return redirect()->back()->with('info', 'Você não ativou sua conta ainda.');
-		}else{
-			if (!Auth::attempt($request->only(['login', 'password']))) {
-				return redirect()->back()->with('info', 'erro ao logar');
-			}
-		}
-
-		$user_type = DB::table('users')->where('login', $request->input('login'))->value('user_type');
-
-		Session::put('user_type', $user_type);
-		return redirect()->route('home')->with('info','Bem vindo');
-	}
-
-	public function getLogout()
-	{
-		Auth::logout();
-
-		return redirect()->route('home')->with('info','Você saiu da sua conta');
-	}
-
-	public function verify($token)
-	{
-	    // The verified method has been added to the user model and chained here
-	    // for better readability
-	    User::where('validation_code',$token)->firstOrFail()->verified();
-	    return redirect()->route('home')->with('info','Conta ativada com sucesso.');
-	}
-
-	
 }
