907df01be824ff92bd8018f4d8a6332bad81a6c7
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-05-25 15:41:59 -0300

    Salvar dados pessoais do candidato.

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -31,14 +31,15 @@ class CandidatoController extends BaseController
 
 	public function getDadosPessoais()
 	{
-		$nome = Auth::user()->nome;
-		$id_user = Auth::user()->id_user;
+		$user = Auth::user();
+		$nome = $user->nome;
+		$id_user = $user->id_user;
 		
 		$candidato = new DadoPessoal();
 		$dados_pessoais = $candidato->retorna_dados_pessoais($id_user);
 
 		if (is_null($dados_pessoais)) {
-			return view('templates.partials.candidato.dados_pessoais')->with('nome', Auth::user()->nome);	
+			return view('templates.partials.candidato.dados_pessoais')->with('nome', $nome);	
 		}else{
 
 			echo "temp";
@@ -59,66 +60,29 @@ class CandidatoController extends BaseController
 			'telefone' => 'required',
 			'celular' => 'required',
 		]);
+
+			$user = Auth::user();
+			$id_user = $user->id_user;
+
+			$user->nome = $request->input('nome');
+			$user->save();
+
+			$candidato = new DadoPessoal();
+
+			$candidato->id_user = $id_user;
+			$candidato->numerorg = $request->input('numerorg');
+			$candidato->emissorrg = $request->input('emissorrg');
+			$candidato->cpf = $request->input('cpf');
+			$candidato->endereco = $request->input('endereco');
+			$candidato->cidade = $request->input('cidade');
+			$candidato->cep = $request->input('cep');
+			$candidato->estado = $request->input('estado');
+			$candidato->telefone = $request->input('telefone');
+			$candidato->celular = $request->input('celular');
+
+			$candidato->save();
 	}
 
-	// public function showNome()
-	// {
-		
 
-	// 	return view('templates.partials.candidato.dados_pessoais')->with('nome', Auth::user()->nome);
-	// }
-
-	// public function postConfiguraMonitoria(Request $request)
-	// {
-
-	// 	$this->validate($request, [
-	// 		'inicio_inscricao' => 'required|date_format:"d/m/Y"|before:fim_inscricao|after:today',
-	// 		'fim_inscricao' => 'required|date_format:"d/m/Y"|after:inicio_inscricao|after:today',
-	// 		'semestre' => 'required',
-	// 		'escolhas_coordenador' => 'required',
-	// 	]);
-    
- //    	$inicio = Carbon::createFromFormat('d/m/Y', $request->inicio_inscricao);
- //    	$fim = Carbon::createFromFormat('d/m/Y', $request->fim_inscricao);
-
- //    	$data_inicio = $inicio->format('Y-m-d');
- //    	$data_fim = $fim->format('Y-m-d');
-
- //    	$ano = $inicio->format('Y');
-
- //    	$monitoria = new ConfiguraInscricao();
-
-	// 	$monitoria->ano_monitoria = $ano;
-	// 	$monitoria->semestre_monitoria = $request->semestre;
-	// 	$monitoria->inicio_inscricao = $data_inicio;
-	// 	$monitoria->fim_inscricao = $data_fim;
-
-	// 	$monitoria->save();
-
-	// 	$id_monitoria=$monitoria->id_monitoria;
-
-	// 	for ($i=0; $i < sizeof($request->escolhas_coordenador); $i++) { 
-
-	// 		$disciplinamonitoria = new DisciplinaMonitoria;
-
-	// 		$disciplinamonitoria->id_monitoria = $id_monitoria;
-			
-	// 		$disciplinamonitoria->codigo_disciplina = $request->escolhas_coordenador[$i];
-
-	// 		$disciplinamonitoria->save();
-
-	// 	}
-
-	// 	return redirect()->route('configura.monitoria')->with('info','Dados gravados com sucesso');
-
-		
-
-	// }
-
-	// public function getRelatorioMonitoria()
-	// {
-
-	// 	return view('templates.partials.coordenador.relatorio_monitoria');
-	// }
 
 }
