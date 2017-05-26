44ba7e95d6461ba370a4071c2b3ecc2dcc1f973a
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-05-26 17:51:48 -0300

    Template para escolhas das monitorias às quais o candidato está aplicando.

---
+++ resources/views/templates/partials/candidato/escolha_monitoria.blade.php
@@ -0,0 +1,118 @@
+@extends('templates.default')
+
+@section('stylesheets')
+  <link href="{{ asset('css/parsley.css') }}" rel="stylesheet">
+@endsection
+
+@section('escolha_monitoria')
+      <form data-parsley-validate="" action="" method="POST" class="form-group" enctype="multipart/form-data">
+      <fieldset class="scheduler-border">
+        <legend class="scheduler-border">Monitorias disponíveis</legend>
+        <h4 align="center">Escolha até três (03) disciplinas, em ordem de prioridade.</h4>
+        <div class="form-inline">
+          <div class="row">
+            <div class="form-group col-xs-6">
+              <label for="email">Disciplina:</label>
+              <select id="id_disciplina" name="{escolha_aluno}" class="form-control">
+                {monitorias_disponiveis}
+              </select>
+            </div>
+
+            <div class="form-group col-xs-6">
+              <!-- BEGIN mencao_aluno -->
+              <label for="email">Menção:</label>
+              <select id="id_mencao" name="{mencao_aluno}" class="form-control">
+                <option selected="selected" value="mencao_vazia">Menção</option>
+                <option value="SS">SS</option>
+                <option value="MS">MS</option>
+                <option value="MM">MM</option>
+              </select>
+            </div>       
+          </div>
+        </div>
+      </fieldset>
+      <fieldset class="scheduler-border">
+          <legend class="scheduler-border">Você será monitor de um projeto de monitoria específico?</legend>
+          <h4>Atenção: Apenas marque SIM se você foi convidado por um professor.</h4>
+          <div class="form-horizontal">
+              <p>
+                <input type="radio" name="monitor_projeto" id="monitor_projeto" value="nao" required="" @if(Request::old('monitor_projeto')=="nao") checked @endif> Não<br>
+                <input type="radio" name="monitor_projeto" id="monitor_projeto" value="sim" @if(Request::old('monitor_projeto')=="sim") checked @endif> Sim. Por favor, digite abaixo o nome do professor que será responsável por sua monitoria. Neste caso, apenas será possível matrícula na opção de monitoria voluntária.<br>
+              </p>
+              <h4>Caso tenha respondido "SIM" à questão anterior, escreva o nome do professor que será responsável por sua monitoria.</h4>
+              <div class="col-md-6">
+                <input id="nome_professor" name="nome_professor" type="text" class="form-control input-md" value="{{Request::old('nome_professor') ?: '' }}>
+              </div>
+            </div>
+        </fieldset>
+
+      <fieldset class="scheduler-border">
+        <legend class="scheduler-border">Tipo de Monitoria</legend>
+          <div class="form-horizontal">
+            <div class="row">
+              <p>
+                <input type="radio" name="tipo_monitoria" id="somentevoluntaria" value="somentevoluntaria" required="" @if(Request::old('tipo_monitoria')=="somentevoluntaria") checked @endif> Somente voluntária<br>
+                <input type="radio" name="tipo_monitoria" id="somenteremunerada" value="somenteremunerada" @if(Request::old('tipo_monitoria')=="somenteremunerada") checked @endif> Somente remunerada<br>
+                <input type="radio" name="tipo_monitoria" id="indiferente" value="indiferente" @if(Request::old('tipo_monitoria')=="indiferente") checked @endif> Indiferente<br>
+              </p>
+          </div>
+        </fieldset>
+    
+        <fieldset class="scheduler-border">
+          <legend class="scheduler-border">Explicite seus dias e horários possíveis para a monitoria:</legend>
+          <div class="form-horizontal">
+            <div class="row">
+              <table class="table table-striped">                     
+                <div class="table responsive">
+                  <thead>
+                    <tr>
+                    <th></th>
+                      <th class="text-center">{cabecalho_hora}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{dia_semana}</td>
+                      <td scope="row" class="text-center">
+                        <input type="radio" name="{nome_hora_monitoria}" id="radio_hora_monitoria" value="{id_hora_1}">
+                      </td>
+                      <td class="text-center">
+                        <input type="radio" name="{nome_hora_monitoria}" id="radio_hora_monitoria" value="{id_hora_2}">
+                      </td>
+                      <td class="text-center">
+                        <input type="radio" name="{nome_hora_monitoria}" id="checkbox_hora_monitoria" value="{id_hora_3}">
+                      </td>
+                    </tr>
+                  </tbody>
+                </div>
+              </table>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset class="scheduler-border">
+        <legend class="scheduler-border">Declaração de conhecimento das regras da Monitoria da UnB:</legend>
+          <div class="row">
+            <p>
+              <input type="checkbox" name="concordatermos" id="concordatermos" value="1" required="">
+              Declaro conhecer os critérios de participação do Programa de Monitoria de Graduação, estabelecidos pela Resolução CEPE no 008/90 de 26.10.1990 (disponível online em <a href="http://tinyurl.com/hg3ch99" target="_blank">http://tinyurl.com/hg3ch99</a>), e ser conhecedor que a participação no Programa não estabelece nenhum vínculo empregatício meu junto a Fundação Universidade de Brasília – UnB.
+            </p>
+          </div>
+      </fieldset>
+
+      <div class="col-xs-12" style="height:35px;"></div>
+        <div class="form-group">
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3 text-center">
+              <input type="submit" name="registrar" id="register-submit" class="btn btn-primary btn-lg" tabindex="4" value="Enviar" disabled="disabled">
+            </div>
+          </div>
+        </div>
+      <input type="hidden" name="_token" value="{{ Session::token() }}">
+    </form>
+@endsection
+
+@section('scripts')
+  <script src="{{ asset('js/parsley.min.js') }}"></script>
+  <script src="{{ asset('i18n/pt-br.js') }}"></script>
+@endsection
