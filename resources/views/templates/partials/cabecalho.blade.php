0f6a1d08c0514f0d41caf6d71f7e4e6fd185d698
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-06-06 15:57:52 -0300

    Logo.

--- resources/views/templates/partials/cabecalho.blade.php
+++ resources/views/templates/partials/cabecalho.blade.php
@@ -3,43 +3,16 @@
     <div class="header-main-content">
       <div class="container-fluid">
         <div class="row-fluid">
-          <div class="span6">
-            <div id="logo"><img src="{{ asset('imagens/logo/logo_unb.png') }}" class="img-responsive" alt="unb"></div>
-          </div>
-          <div class="span6">
-            <div class="clearfix"></div>
-          </div>
-                   </div>
+        
+              <div class="col-sm-6 col-md-4 col-xs-12 image-container">
+                <img src="{{ asset('imagens/logo/logo_unb.png') }}" class="img-responsive" style="height:120px" />
+              </div>
+              <div class="col-sm-6 col-md-8 col-xs-12">  
+                <h1>Inscrições Monitoria/MAT</h1>
+                <h2>{{$periodo_inscricao}}</h2>
+              </div>
       </div>
     </div>
   </div>
 
 </header>
-
-{{-- <div class="container">
-    <div class="row">
-      <div class="jumbotron col-xs-12">
-        <div class="page-header">
-          <h1 class="col-xs-12">Departamento de Matemática</h1> 
-          <h2>Inscrições para a Monitoria do MAT: {{$periodo_inscricao}}</h2> 
-        </div>
-      </div>
-    </div>
-</div> --}}
-
-{{-- <div class="col-sm-6 col-md-6 col-xs-6">
-
-              <div class="thumbnail" style="border:none; background:white;">
-
-              <div class="col-sm-6 col-md-4 col-xs-12 image-container">
-                <img src="{{ asset('imagens/logo/logo_unb.jpg') }}" style="height:100px; margin-left:-15px;" />
-              </div>
-
-              <div class="col-sm-6 col-md-8 col-xs-12">  
-
-                <h3>Inscrições Monitoria/MAT</h3>
-                 <p style="font-size:10px; color:#03225C;">{{$periodo_inscricao}}</p>
-              </div>
-              </div>
-
- </div> --}}
