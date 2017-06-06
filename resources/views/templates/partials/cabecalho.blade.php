3c7c993cbfc1241cf70428dac3adb2d614a9b154
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-06 12:03:19 -0300

    Novo cabeçalho.

--- resources/views/templates/partials/cabecalho.blade.php
+++ resources/views/templates/partials/cabecalho.blade.php
@@ -1,4 +1,22 @@
-<div class="container">
+<header id="header">
+  <div class="header-main">
+    <div class="header-main-content">
+      <div class="container-fluid">
+        <div class="row-fluid">
+          <div class="span6">
+            <div id="logo"><a href="http://localhost:8083/moodle"><img src="//localhost:8083/moodle/pluginfile.php/1/theme_klass/logo/1487030465/logo-unb-top.png" class="img-responsive" alt="Klass"></a></div>
+          </div>
+          <div class="span6">
+            <div class="clearfix"></div>
+          </div>
+                   </div>
+      </div>
+    </div>
+  </div>
+
+</header>
+
+{{-- <div class="container">
     <div class="row">
       <div class="jumbotron col-xs-12">
         <div class="page-header">
@@ -7,4 +25,4 @@
         </div>
       </div>
     </div>
-</div>
+</div> --}}
