327c87356f6b87e7141075d9299dae0bec64ad87
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-06-06 18:42:42 -0300

    Estilização.

--- resources/views/templates/partials/cabecalho.blade.php
+++ resources/views/templates/partials/cabecalho.blade.php
@@ -3,13 +3,13 @@
     <div class="header-main-content">
       <div class="container-fluid">
         <div class="row-fluid">
-        
-              <div class="col-sm-6 col-md-4 col-xs-12 image-container">
+              <div class="col-sm-4 col-md-4 col-xs-12 image-container">
                 <img src="{{ asset('imagens/logo/logo_unb.png') }}" class="img-responsive" style="height:120px" />
               </div>
-              <div class="col-sm-6 col-md-8 col-xs-12">  
-                <h1>Inscrições Monitoria/MAT</h1>
-                <h2>{{$periodo_inscricao}}</h2>
+              <div class="col-sm-8 col-md-8 col-xs-12">  
+              <h1>Departamento de Matemática</h1>
+              <h2>Inscrições Monitoria</h2>
+              <h3>{{$periodo_inscricao}}</h3>
               </div>
       </div>
     </div>
--- resources/views/templates/partials/rodape.blade.php
+++ resources/views/templates/partials/rodape.blade.php
@@ -5,18 +5,7 @@
         
   <div class="footer-foot">
     <div class="container-fluid">
-              <p>Copyright &copy; 2015 - Developed by <a href="http://lmsace.com">LMSACE.com</a>. Powered by <a href="https://moodle.org">Moodle</a></p>
+              <p>MAT/UnB</p>
            </div>
   </div>
 </footer>
-
-{{-- <div class="container">
-    <div class="row">
-      <div class="jumbotron col-xs-12">
-        <div class="page-header">
-          <h1 class="col-xs-12">Departamento de Matemática</h1> 
-          <h2>Inscrições para a Monitoria do MAT: {{$periodo_inscricao}}</h2> 
-        </div>
-      </div>
-    </div>
-</div> --}}
