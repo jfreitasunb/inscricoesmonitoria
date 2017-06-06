87b9ee218ec1f966853b80b86a9d218a62e9dbcd
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-06-06 18:45:39 -0300

    Inserção de ano no rodapé.

--- resources/views/templates/partials/rodape.blade.php
+++ resources/views/templates/partials/rodape.blade.php
@@ -2,10 +2,10 @@
   <div class="footer-main">
     <div class="container-fluid">
       <div class="row-fluid">
-        
   <div class="footer-foot">
     <div class="container-fluid">
-              <p>MAT/UnB</p>
-           </div>
+<p>MAT/UnB - <?php echo date("Y");?></p>
+</div>
+  </div>
   </div>
 </footer>
