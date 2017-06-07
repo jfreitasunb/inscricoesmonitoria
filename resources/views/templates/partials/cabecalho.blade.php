918e67ce3622953307f6c4bb940f909801c12c7c
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-07 12:39:17 -0300

    Formatação do texto.

--- resources/views/templates/partials/cabecalho.blade.php
+++ resources/views/templates/partials/cabecalho.blade.php
@@ -3,16 +3,17 @@
     <div class="header-main-content">
       <div class="container-fluid">
         <div class="row-fluid">
-              <div class="col-sm-4 col-md-4 col-xs-12 image-container">
+              <div class="col-sm-4 col-md-3 col-xs-12 image-container">
                 <img src="{{ asset('imagens/logo/logo_unb.png') }}" class="img-responsive" style="height:120px" />
               </div>
-              <div class="col-sm-8 col-md-8 col-xs-12">  
+              <div class="col-sm-8 col-md-7 col-xs-12">  
               <h1>Departamento de Matemática</h1>
               <h2>Inscrições Monitoria</h2>
               <h3>{{$periodo_inscricao}}</h3>
               </div>
       </div>
     </div>
+  </div>
   </div>
 
 </header>
