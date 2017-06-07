327bde22132e2e0112955ace2335149c0fce3614
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-07 12:39:40 -0300

    Melhorando a aparência do rodapé.

--- resources/views/templates/partials/rodape.blade.php
+++ resources/views/templates/partials/rodape.blade.php
@@ -1,11 +1,11 @@
 <footer id="footer">
   <div class="footer-main">
-    <div class="container-fluid">
-      <div class="row-fluid">
-  <div class="footer-foot">
-    <div class="container-fluid">
-<p>MAT/UnB - <?php echo date("Y");?></p>
-</div>
-  </div>
+  	<div class="footer-foot">
+  		<div class="container-fluid">
+  			<div class="row-fluid">
+				<p>MAT/UnB - <?php echo date("Y");?></p>
+			</div>
+  		</div>
+  	</div>
   </div>
 </footer>
