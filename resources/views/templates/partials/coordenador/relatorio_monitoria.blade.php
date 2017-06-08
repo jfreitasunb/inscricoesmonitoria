95ca7e8cb04db8eac8ede86de6f39096bee123c7
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-08 20:51:45 -0300

    Identação.

--- resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
@@ -3,29 +3,27 @@
 @section('relatorio_monitoria')
   <form action="" method="POST">
     <legend>Relatório de inscritos</legend>
-	
-    <table class="table table-striped">
-  <thead>
-    <tr>
-      <th>Id Monitoria</th>
-      <th>Ano</th>
-      <th>Semestre</th>
-      <th>Início</th>
-      <th>Fim</th>
-    </tr>
-  </thead>
-  <tbody>
-  	@foreach($relatorio_disponivel as $relatorio)
-    <tr>
-      <th scope="row"><a href="{{ route('gera.monitoria') }}">{{$relatorio->id_monitoria}}</a></th>
-      <td><a href="{{ route('gera.monitoria') }}">{{$relatorio->ano_monitoria}}</a></td>
-      <td><a href="{{ route('gera.monitoria') }}">{{$relatorio->semestre_monitoria}}</a></td>
-      <td><a href="{{ route('gera.monitoria') }}">{{$relatorio->inicio_inscricao}}</a></td>
-      <td><a href="{{ route('gera.monitoria') }}">{{$relatorio->fim_inscricao}}</a></td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
+	    <table class="table table-striped">
+		  	<thead>
+			    <tr>
+			      <th>Id Monitoria</th>
+			      <th>Ano</th>
+			      <th>Semestre</th>
+			      <th>Início</th>
+			      <th>Fim</th>
+			    </tr>
+		  	</thead>
+		  	<tbody>
+		  		@foreach($relatorio_disponivel as $relatorio)
+			    	<tr>
+			      	<th scope="row"><a href="{{ route('gera.monitoria') }}">{{$relatorio->id_monitoria}}</a></th>
+			      	<td><a href="{{ route('gera.monitoria') }}">{{$relatorio->ano_monitoria}}</a></td>
+			      	<td><a href="{{ route('gera.monitoria') }}">{{$relatorio->semestre_monitoria}}</a></td>
+			      	<td><a href="{{ route('gera.monitoria') }}">{{$relatorio->inicio_inscricao}}</a></td>
+			      	<td><a href="{{ route('gera.monitoria') }}">{{$relatorio->fim_inscricao}}</a></td>
+			    	</tr>
+		    	@endforeach
+		  	</tbody>
+		</table>
   </form>
-
 @stop
