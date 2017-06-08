fb6dce9bed7125b8a3791bba704ba2d182c1417c
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-08 20:46:06 -0300

    Tabela com as inscrições disponíveis para criação dos relatórios.

--- resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
@@ -3,6 +3,29 @@
 @section('relatorio_monitoria')
   <form action="" method="POST">
     <legend>Relatório de inscritos</legend>
+	
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Id Monitoria</th>
+      <th>Ano</th>
+      <th>Semestre</th>
+      <th>Início</th>
+      <th>Fim</th>
+    </tr>
+  </thead>
+  <tbody>
+  	@foreach($relatorio_disponivel as $relatorio)
+    <tr>
+      <th scope="row">{{$relatorio->id_monitoria}}</th>
+      <td>{{$relatorio->ano_monitoria}}</td>
+      <td>{{$relatorio->semestre_monitoria}}</td>
+      <td>{{$relatorio->inicio_inscricao}}</td>
+      <td>{{$relatorio->fim_inscricao}}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
   </form>
 
 @stop
