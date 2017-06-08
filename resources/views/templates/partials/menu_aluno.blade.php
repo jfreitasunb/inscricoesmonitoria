ade18e37d0a141a4395f63e9b8dca2160db98516
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-06-08 15:54:21 -0300

    Correção de erros de digitação.

--- resources/views/templates/partials/menu_aluno.blade.php
+++ resources/views/templates/partials/menu_aluno.blade.php
@@ -10,11 +10,11 @@
     </div>
     <div class="collapse navbar-collapse" id="bar1">
       <ul class="nav navbar-nav">
-        <li"class="{{ Route::currentRouteNamed('dados.pessoais') ? 'active' : '' }}"><a href="{{ route('dados.pessoais') }}">Dados Pessoais</a></li>
-        <liclass="{{ Route::currentRouteNamed('dados.bancarios') ? 'active' : '' }}"><a href="{{ route('dados.bancarios') }}">Dados Bancários</a></li>
-        <liclass="{{ Route::currentRouteNamed('dados.academicos') ? 'active' : '' }}"><a href="{{ route('dados.academicos') }}">Dados Acadêmicos</a></li>
-        <liclass="{{ Route::currentRouteNamed('dados.escolhas') ? 'active' : '' }}"><a href="{{ route('dados.escolhas') }}">Escolher Monitoria</a></li>
-        <liclass="{{ Route::currentRouteNamed('auth.logout') ? 'active' : '' }}"><a href="{{ route('auth.logout') }}">Sair</a></li>
+        <li class="{{ Route::currentRouteNamed('dados.pessoais') ? 'active' : '' }}"><a href="{{ route('dados.pessoais') }}">Dados Pessoais</a></li>
+        <li class="{{ Route::currentRouteNamed('dados.bancarios') ? 'active' : '' }}"><a href="{{ route('dados.bancarios') }}">Dados Bancários</a></li>
+        <li class="{{ Route::currentRouteNamed('dados.academicos') ? 'active' : '' }}"><a href="{{ route('dados.academicos') }}">Dados Acadêmicos</a></li>
+        <li class="{{ Route::currentRouteNamed('dados.escolhas') ? 'active' : '' }}"><a href="{{ route('dados.escolhas') }}">Escolher Monitoria</a></li>
+        <li class="{{ Route::currentRouteNamed('auth.logout') ? 'active' : '' }}"><a href="{{ route('auth.logout') }}">Sair</a></li>
       </ul>
     </div>
   </nav>
