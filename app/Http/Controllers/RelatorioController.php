46881898651be8e1657e9f0828a2794fce231b4f
VALINOR\jfreitas <jfreitas@mat.unb.br>
2017-06-12 14:46:31 -0300

    Remoçãod e função desnecessária.

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -48,52 +48,7 @@ class RelatorioController extends BaseController
        
     }
 
-	public function postConfiguraMonitoria(Request $request)
-	{
-
-		$this->validate($request, [
-			'inicio_inscricao' => 'required|date_format:"d/m/Y"|before:fim_inscricao|after:today',
-			'fim_inscricao' => 'required|date_format:"d/m/Y"|after:inicio_inscricao|after:today',
-			'semestre' => 'required',
-			'escolhas_coordenador' => 'required',
-		]);
-    
-    	$inicio = Carbon::createFromFormat('d/m/Y', $request->inicio_inscricao);
-    	$fim = Carbon::createFromFormat('d/m/Y', $request->fim_inscricao);
-
-    	$data_inicio = $inicio->format('Y-m-d');
-    	$data_fim = $fim->format('Y-m-d');
-
-    	$ano = $inicio->format('Y');
-
-    	$monitoria = new ConfiguraInscricao();
-
-		$monitoria->ano_monitoria = $ano;
-		$monitoria->semestre_monitoria = $request->semestre;
-		$monitoria->inicio_inscricao = $data_inicio;
-		$monitoria->fim_inscricao = $data_fim;
-
-		$monitoria->save();
-
-		$id_monitoria=$monitoria->id_monitoria;
-
-		for ($i=0; $i < sizeof($request->escolhas_coordenador); $i++) { 
-
-			$disciplinamonitoria = new DisciplinaMonitoria;
-
-			$disciplinamonitoria->id_monitoria = $id_monitoria;
-			
-			$disciplinamonitoria->codigo_disciplina = $request->escolhas_coordenador[$i];
-
-			$disciplinamonitoria->save();
-
-		}
-
-		return redirect()->route('configura.monitoria')->with('info','Dados gravados com sucesso');
-
-		
-
-	}
+	
 
 	public function getRelatorioMonitoria()
 	{
