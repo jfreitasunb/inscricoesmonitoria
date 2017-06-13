239ea8366390f110a2c203b1d0482e4ba4ab1824
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-12 22:20:38 -0300

    Gravando dados em arquivo CSV.

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -22,6 +22,7 @@ use Monitoriamat\Http\Controllers\Controller;
 use Monitoriamat\Http\Controllers\AuthController;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use League\Csv\Writer;
+use Storage;
 
 /**
 * Classe para visualização da página inicial.
@@ -39,23 +40,66 @@ class RelatorioController extends BaseController
 		return view('templates.partials.coordenador.relatorio_monitoria')->with('relatorio_disponivel', $relatorio_disponivel);
 	}
 
-	public function geraRelatorio($id_monitoria){
+	public function geraRelatorio($id_monitoria)
+       {
 
-	   Storage::disk('csv')->put('file.csv', $content);
-	   $content = Storage::disk('csv')->get('file.csv');
-       $relatorio = new FinalizaEscolha();
-       $usuarios_finalizados = $relatorio->retorna_usuarios_relatorios($id_monitoria);
+       	$arquivo_relatorio = "Relatorio_inscritos_".$id_monitoria.".csv";
 
-       foreach ($usuarios_finalizados as $usuario) {
 
+              $file_path = storage_path()."/csv/";
+
+              $csv_relatorio = Writer::createFromPath($file_path.$arquivo_relatorio, 'w+');
+
+
+              $relatorio = new FinalizaEscolha();
+              $usuarios_finalizados = $relatorio->retorna_usuarios_relatorios($id_monitoria);
+
+              $cabecalho = ["Nome","E-mail","Celular","Curso de Graduação", "IRA", "Tipo de Monitoria", "Monitor Convidado", "Nome do Professor", "Escolhas", "Horários", "Atuações Anteoriores"];
+
+              $csv_relatorio->insertOne($cabecalho);
+
+
+              foreach ($usuarios_finalizados as $usuario) {
+
+                     $linha_arquivo = [];
        		$id_user = $usuario->id_user;
+
+                     $user = New User();
+
+                     $email = $user->find($id_user)->email;
 
        		$dado_pessoal = new DadoPessoal();
        		$dados_pessoais = $dado_pessoal->retorna_dados_pessoais($id_user);
-       		
+
+                     $linha_arquivo['nome'] = $dados_pessoais->nome;
+                     
+                     $linha_arquivo['email'] = $email;
+
+                     $linha_arquivo['celular'] = $dados_pessoais->celular;
+
        		$dado_academico = new DadoAcademico();
 
        		$dados_academicos = $dado_academico->retorna_dados_academicos($id_user);
+
+                     $linha_arquivo['curso_graduacao'] = $dados_academicos->curso_graduacao;
+
+                     $linha_arquivo['ira'] = $dados_academicos->ira;
+
+                     $linha_arquivo['tipo_monitoria'] = $usuario->tipo_monitoria;
+
+                     if ($dados_academicos->monitor_convidado) {
+                            $linha_arquivo['monitor_convidado'] = $dados_academicos->monitor_convidado;
+
+                            $linha_arquivo['nome_professor'] = $dados_academicos->nome_professor;
+
+                     }else{
+                            
+                            $linha_arquivo['monitor_convidado'] = "Não";
+
+                            $linha_arquivo['nome_professor'] = "";
+                     }
+                     
+
 
        		$escolheu = new EscolhaMonitoria();
 
@@ -72,7 +116,8 @@ class RelatorioController extends BaseController
 
        			$nome = $nome_disciplina[0]->nome;
 
-       			$linha_escolhas[] = $nome.";".$escolhas_candidato[$i]->mencao_aluno;
+       			$linha_arquivo['escolha_'.($i+1)] = $nome.",".$escolhas_candidato[$i]->mencao_aluno;
+
        		}
 
        		$horario = new HorarioEscolhido();
@@ -81,12 +126,15 @@ class RelatorioController extends BaseController
 
        		for ($j=0; $j < sizeof($horarios_escolhidos); $j++) { 
        			
-       			$linha_horario_escolhidos[] = $horarios_escolhidos[$j]->dia_semana.";".$horarios_escolhidos[$j]->horario_monitoria;
+       			$linha_arquivo['horario'.($j+1)] = $horarios_escolhidos[$j]->dia_semana.",".$horarios_escolhidos[$j]->horario_monitoria;
        		}
 
 
+                     $csv_relatorio->insertOne($linha_arquivo);
 
-       }
+
+
+              }
        
     }
 
