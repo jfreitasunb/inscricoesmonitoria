c81adb4072ff0b976baf7a35415d5f175cdd2885
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-13 21:59:01 -0300

    Formatando a tabela de relatórios.

--- resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
@@ -8,8 +8,7 @@
 			    <tr>
 			      <th>Id Monitoria</th>
 			      <th>Ano/Semestre</th>
-			      <th>Início</th>
-			      <th>Fim</th>
+			      <th>Período de Inscrição</th>
 			      <th>Relatório de Inscritos</th>
 			      <th>Documentos</th>
 			    </tr>
@@ -19,8 +18,7 @@
 			    	<tr>
 			      	<th scope="row"><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->id_monitoria}}</a></th>
 			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->ano_monitoria."/".$relatorio->semestre_monitoria}}</a></td>
-			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->inicio_inscricao}}</a></td>
-			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->fim_inscricao}}</a></td>
+			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{\Carbon\Carbon::parse($relatorio->inicio_inscricao)->format('d/m/Y')." à ".\Carbon\Carbon::parse($relatorio->fim_inscricao)->format('d/m/Y')}}</a></td>
 			      	<td>@if($monitoria == $relatorio->id_monitoria) <a target="_blank" href="{{asset('relatorios/csv/'.$arquivo_relatorio)}}" >{{$arquivo_relatorio}}</a> @endif</td>
 			      	<td>@if($monitoria == $relatorio->id_monitoria) <a target="_blank" href="{{asset('relatorios/zip/'.$documentos_zipados)}}">{{$documentos_zipados}}</a> @endif</td>
 			    	</tr>
