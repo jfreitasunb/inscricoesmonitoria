6cb525014b0b04b687a7dda3e9634375f5388c64
José Antônio O Freitas <jfreitas@mat.unb.br>
2017-06-13 21:48:13 -0300

    Melhorando a aparência.

--- resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_monitoria.blade.php
@@ -7,8 +7,7 @@
 		  	<thead>
 			    <tr>
 			      <th>Id Monitoria</th>
-			      <th>Ano</th>
-			      <th>Semestre</th>
+			      <th>Ano/Semestre</th>
 			      <th>Início</th>
 			      <th>Fim</th>
 			      <th>Relatório de Inscritos</th>
@@ -19,8 +18,7 @@
 		  		@foreach($relatorio_disponivel as $relatorio)
 			    	<tr>
 			      	<th scope="row"><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->id_monitoria}}</a></th>
-			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->ano_monitoria}}</a></td>
-			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->semestre_monitoria}}</a></td>
+			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->ano_monitoria."/".$relatorio->semestre_monitoria}}</a></td>
 			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->inicio_inscricao}}</a></td>
 			      	<td><a href="{!! route('gera.relatorio', ['id_monitoria' => $relatorio->id_monitoria]) !!}">{{$relatorio->fim_inscricao}}</a></td>
 			      	<td>@if($monitoria == $relatorio->id_monitoria) <a target="_blank" href="{{asset('relatorios/csv/'.$arquivo_relatorio)}}" >{{$arquivo_relatorio}}</a> @endif</td>
