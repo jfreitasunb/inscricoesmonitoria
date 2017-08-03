e560e9b57af95dfd57a4762d902f6573db7512f2
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-08-03 15:07:46 -0300

    Mudança no middleware de tipo de usuário para permitir múltiplos tipos em determinadas rotas.

--- app/Http/Middleware/UserRoles.php
+++ app/Http/Middleware/UserRoles.php
@@ -17,17 +17,22 @@ class UserRoles
     public function handle($request, Closure $next, ...$roles)
     {
         
-        foreach ($roles as $role ) {
-            if (!Auth::check()){
-                if (Auth::user()->user_type <> $role) {
-                     return redirect('/');
+        $libera_acesso = 0;
+        
+        if (Auth::check()){
+            foreach ($roles as $role ) {
+                if (Auth::user()->user_type == $role) {
+                    $libera_acesso = 1;
                 }
-            } 
-               
             }
-            return $next($request);
+        }else{
+            return redirect('/');
         }
 
+        if (!$libera_acesso) {
+            return redirect('/');
+        }
         
+        return $next($request);
     }
 }
