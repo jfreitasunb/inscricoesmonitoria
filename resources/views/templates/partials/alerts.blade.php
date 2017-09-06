75f534a531136f44eab0a3d72f713404147bb6b8
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-06 15:58:35 -0300

    Usando o sweetalert2 e notify para exibir as mensagens.

--- resources/views/templates/partials/alerts.blade.php
+++ resources/views/templates/partials/alerts.blade.php
@@ -1,31 +1,5 @@
-@if(Session::has('info'))
-	<div class="alert alert-info" role="alert">
-		<a href="#" class="close" data-dismiss="alert">&times;</a>
-		{{ Session::get('info')}}
-	</div>
-@endif
-
-@if(Session::has('success'))
-	<div class="alert alert-success" role="alert">
-		<a href="#" class="close" data-dismiss="alert">&times;</a>
-		{{ Session::get('success')}}
-	</div>
-@endif
-
-@if(Session::has('erro'))
-	<div class="alert alert-danger" role="alert">
-		<a href="#" class="close" data-dismiss="alert">&times;</a>
-		{{ Session::get('erro')}}
-	</div>
-@endif
-
-@if (count($errors) > 0)
-	<div class="alert alert-danger" role="alert">
-		<a href="#" class="close" data-dismiss="alert">&times;</a>
-		<ul>
-			@foreach ($errors->all() as $error)
-				<li> {{ $error }}</li>
-			@endforeach
-		</ul>
+@if (notify()->ready())
+	<div class="alert alert-{{ notify()->type() }}">
+		{{ notify()->message() }}
 	</div>
 @endif
